5aa266d6ced52fd745c6fab05d6501d75b434efb
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2026-07-02 17:16:50 +0100

    Create a component for the general information portion of the form

--- src/components/general-info.jsx
+++ src/components/general-info.jsx
@@ -1,2 +1,72 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "../styles/general-info.css";
+
+function PersonalInfo() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    location: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  return (
+    <section>
+      <label htmlFor="firstName">First Name:</label>
+      <input
+        name="firstName"
+        id="firstName"
+        type="text"
+        minLength={3}
+        required
+        value={formData.firstName}
+        onChange={handleChange}
+      />
+      <label htmlFor="lastName">Last Name:</label>
+      <input
+        name="lastName"
+        id="lastName"
+        type="text"
+        minLength={3}
+        required
+        value={formData.lastName}
+        onChange={handleChange}
+      />
+      <label htmlFor="email">Email address:</label>
+      <input
+        name="email"
+        id="email"
+        type="email"
+        placeholder="abc@example.com"
+        required
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="phoneNumber">Phone number:</label>
+      <input
+        name="phoneNumber"
+        id="phoneNumber"
+        type="tel"
+        value={formData.phoneNumber}
+        onChange={handleChange}
+      />
+      <label htmlFor="location">Location:</label>
+      <input
+        name="location"
+        id="location"
+        type="text"
+        placeholder="eg. Tokyo, Japan"
+        pattern="[A-Za-z\s.-]+,\s*[A-Za-z\s.-]+"
+        value={formData.location}
+        onChange={handleChange}
+      />
+    </section>
+  );
+}
+
+export default PersonalInfo;
