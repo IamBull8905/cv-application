f533c73b8aec356908e90b22f2678aa20dca0529
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2026-07-02 17:43:33 +0100

    Build the education experience component
    This commit has additional features awaiting to be implemented such as end date changing to "present" if currently studying and the ability to add more than one education institution.

--- src/components/education-exp.jsx
+++ src/components/education-exp.jsx
@@ -1,2 +1,68 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "../styles/education-exp.css";
+
+function EducationExperience() {
+  const [formData, setFormData] = useState({
+    schoolName: "",
+    titleOfStudy: "",
+    startDate: "",
+    endDate: "",
+    currentlyStudying: false,
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  return (
+    <section>
+      <label htmlFor="schoolName">School Name:</label>
+      <input
+        name="schoolName"
+        id="schoolName"
+        type="text"
+        maxLength={50}
+        required
+        value={formData.schoolName}
+        onChange={handleChange}
+      />
+      <label htmlFor="titleOfStudy">Title of study:</label>
+      <input
+        name="titleOfStudy"
+        id="titleOfStudy"
+        type="text"
+        required
+        value={formData.titleOfStudy}
+        onChange={handleChange}
+      />
+      <label htmlFor="startDate">Start date:</label>
+      <input
+        name="startDate"
+        id="startDate"
+        type="date"
+        required
+        value={formData.startDate}
+        onChange={handleChange}
+      />
+      <label htmlFor="endDate">End date:</label>
+      <input
+        name="endDate"
+        id="endDate"
+        type="date"
+        value={formData.endDate}
+        onChange={handleChange}
+      />
+      <label htmlFor="currentlyStudying">Currently studying:</label>
+      <input
+        name="currentlyStudying"
+        id="currentlyStudying"
+        type="checkbox"
+        checked={formData.currentlyStudying}
+        onChange={(event) => setFormData({...formData, currentlyStudying: event.target.checked})}
+      />
+    </section>
+  );
+}
+
+export default EducationExperience;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import PersonalInfo from './components/general-info'
+import EducationExperience from './components/education-exp'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <PersonalInfo></PersonalInfo>
+    <EducationExperience></EducationExperience>
   </StrictMode>,
 )
